5553148fd86bfd4b07fbdfcf145ada83eeec11b9
kribdyr <71062475+kribdyr@users.noreply.github.com>
2025-12-18 13:07:39 +0100

    Add project files.

---
+++ Art.cs
@@ -0,0 +1,28 @@
+﻿namespace ArtLib
+{
+    public class Art
+    {
+        int id;
+        string artName;
+        string artType;
+        double price;
+
+        public Art()
+        {
+            artName = string.Empty;
+            artType = string.Empty;
+            price = 0;
+        }
+
+        public int Id { get; set; }
+        public string ArtName { get { return artName; } set { artName = value; } }
+        public string ArtType { get { return artType; } set {artType = value; } }
+        public double Price { get { return price; } set { price = value; } }
+
+        public override string ToString()
+        {
+            return $"(ID: {id}\n Name: {artName}\n Type: {artType}\n Price: {price})";
+        }
+
+    }
+}
---
+++ ArtRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtLib
+{
+    public class ArtRepository
+    {
+
+        public List<Art> arts;
+        public int nextId = 1;
+
+        public ArtRepository()
+        {
+            arts = new List<Art>();
+
+            Add(new Art { ArtName = "Mona Lisa", ArtType = "Painting", Price = 1000000 });
+            Add(new Art { ArtName = "The Thinker", ArtType = "Sculpture", Price = 500000 });
+            Add(new Art { ArtName = "Starry Night", ArtType = "Painting", Price = 750000 });
+            Add(new Art { ArtName = "The Kiss", ArtType = "Painting", Price = 600000 });
+        }
+
+        public void Add(Art art)
+        {
+            art.Id = nextId;
+            nextId++;
+            arts.Add(art);
+        }
+
+        public List<Art> GetAll()
+        {
+
+            return new List<Art>(arts);
+        }
+
+        public Art GetById(int id)
+        {
+            foreach (var art in arts)
+            {
+                if (art.Id == id)
+                {
+                    return (Art) art;
+                }                
+            }
+            return null;
+        }
+
+        public Art? DeleteArt(int id)
+        {
+
+            Art artToRemove = GetById(id);
+
+            if (artToRemove == null)
+            {
+                return null;
+            }
+            arts.Remove(artToRemove);
+            return artToRemove;
+        }
+
+    }
+}
